bdfe0eaaf80fbf547ce58235f8188ea9bc80884b
carolyn <carolyn.sydneus2028@gbwl.org>
2026-09-18 16:34:56 +0000

    added info

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 /*
 
 This is my comment space!
+-----------------------------
+Algorithm: a step by step proccess to accomplish a task.
+Pseudocode: simplified code to outline programs/algorithms
+   ex: add func()
+      num1
+      num2
+      1 + 2
+      1 + 2 prints
+Sequencing: order of steps 
+------------------------------
+What is the difference between Javascript and Java?
+   Java is a coding language that helps build complex applications like desktop software, mobile apps, and video games while JavaScript is a front-end programming language that adds the interactivity to website/ web pages. JavaScript is more lightweight compared to Java.
 
 */
 
