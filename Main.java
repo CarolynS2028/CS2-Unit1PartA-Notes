fac6e0fac21e8aaabac22f6427c93b0cfddd5232
carolyn <carolyn.sydneus2028@gbwl.org>
2026-09-22 13:41:00 +0000

    made notes

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 /*
 
 This is my comment space!
------------------------------
+------------------------------
 Algorithm: a step by step proccess to accomplish a task.
 Pseudocode: simplified code to outline programs/algorithms
    ex: add func()
@@ -13,14 +13,32 @@ Sequencing: order of steps
 ------------------------------
 What is the difference between Javascript and Java?
    Java is a coding language that helps build complex applications like desktop software, mobile apps, and video games while JavaScript is a front-end programming language that adds the interactivity to website/ web pages. JavaScript is more lightweight compared to Java.
+-----------------------------
+Notes
+
+Object-oriented Programming: programming built on classes and objects
+Class: blueprint of an object
+   - no memory
+Object: actual implementation
+   - sorted memory
+Method: reusable chunk of code that accomplishes an action
+   - main() entry point to our code
+*We code in a IDE with a complier 
+Compiler: translates our Java to binary
+*Every action in java ends with a ";"
 
 */
 
-
+// line comment *
 
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Hi there!");
+      System.out.println("Hi there!");
+      System.out.println("It makes no sense to divide a number by zero!");
+      System.out.println(3/0);
+
+
    }
 }
